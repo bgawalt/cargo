dc00215e2641feba62db68351c6a951f80d5ab51
Brian Gawalt <bgawalt@gmail.com>
2017-03-29 21:57:14 -0700

    First commit.

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "cargo"
+
+version := "1.0"
---
+++ src/main/scala/com/gawalt/cargo/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.gawalt.cargo
+
+/**
+ * This source file created by Brian Gawalt, Mar 29, 2017.
+ * It is subject to the MIT license bundled with this package in the file LICENSE.txt.
+ * Copyright (c) Brian Gawalt, 2017
+ */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world!!!")
+  }
+}
